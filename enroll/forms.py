5f27b2ec6ef0dd6c4178b91fd60cbb18075927ea
robinNcode <msmrobin518@gmail.com>
2022-05-24 03:11:07 +0600

    dynamically form generated with bs

--- enroll/forms.py
+++ enroll/forms.py
@@ -2,11 +2,16 @@ from django.core import validators
 from django import forms
 from .models import User
 
+
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['name', 'password', 'email', 'confirm_password']
+        fields = ['name', 'email', 'password', 'confirm_password']
         widgets = {
-            'password': forms.PasswordInput(),
-            'confirm_password': forms.PasswordInput(),
+            'name': forms.TextInput(attrs={'class': 'form-control form-group', 'placeholder': 'Enter your name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control form-group', 'placeholder': 'Enter your email'}),
+            'password': forms.PasswordInput(
+                attrs={'class': 'form-control form-group', 'placeholder': 'Enter your password'}),
+            'confirm_password': forms.PasswordInput(
+                attrs={'class': 'form-control form-group', 'placeholder': 'Confirm your password'}),
         }
